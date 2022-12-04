0efd8fe2d242b785e6696d5038a7d8c546915445
milliamper <szalaimarci99@gmail.com>
2022-12-04 13:42:03 +0100

    Day 3 completed

--- Day_3/day3.py
+++ Day_3/day3.py
@@ -7,14 +7,69 @@ def rucksack_reorganization(filename):
     list_of_words = []
     first_half = []
     second_half = []
-    value = 0
     sum_of_values = 0
+
+    my_dict = {
+        'a': 1,
+        'b': 2,
+        'c': 3,
+        'd': 4,
+        'e': 5,
+        'f': 6,
+        'g': 7,
+        'h': 8,
+        'i': 9,
+        'j': 10,
+        'k': 11,
+        'l': 12,
+        'm': 13,
+        'n': 14,
+        'o': 15,
+        'p': 16,
+        'q': 17,
+        'r': 18,
+        's': 19,
+        't': 20,
+        'u': 21,
+        'v': 22,
+        'w': 23,
+        'x': 24,
+        'y': 25,
+        'z': 26,
+
+        'A': 27,
+        'B': 28,
+        'C': 29,
+        'D': 30,
+        'E': 31,
+        'F': 32,
+        'G': 33,
+        'H': 34,
+        'I': 35,
+        'J': 36,
+        'K': 37,
+        'L': 38,
+        'M': 39,
+        'N': 40,
+        'O': 41,
+        'P': 42,
+        'Q': 43,
+        'R': 44,
+        'S': 45,
+        'T': 46,
+        'U': 47,
+        'V': 48,
+        'W': 49,
+        'X': 50,
+        'Y': 51,
+        'Z': 52,
+    }
 
     with open(filename) as f:
         for line in f:
             lines.append(line.rstrip())
 
-    for i in range(0, 10):
+    for i in range(0, len(lines)):
         list_of_words = list(lines[i])
 
         first_half = list_of_words[:len(list_of_words)//2]
@@ -22,20 +77,84 @@ def rucksack_reorganization(filename):
 
         same_element = list(set(first_half).intersection(second_half))
 
-        if (same_element[0].islower):
-            value = ord(same_element[0])-96
-        else:
-            value = ord(same_element[0])-38
+        sum_of_values += my_dict[same_element[0]]
 
-        print("same: ", same_element[0])
-        print("value: ", value)
-        sum_of_values += value
-        value = 0
-        first_half.clear()
-        second_half.clear()
+    print(sum_of_values)
+
+
+def rucksack_reorganization_part2(filename):
+
+    lines = []
+    sum_of_values = 0
+
+    my_dict = {
+        'a': 1,
+        'b': 2,
+        'c': 3,
+        'd': 4,
+        'e': 5,
+        'f': 6,
+        'g': 7,
+        'h': 8,
+        'i': 9,
+        'j': 10,
+        'k': 11,
+        'l': 12,
+        'm': 13,
+        'n': 14,
+        'o': 15,
+        'p': 16,
+        'q': 17,
+        'r': 18,
+        's': 19,
+        't': 20,
+        'u': 21,
+        'v': 22,
+        'w': 23,
+        'x': 24,
+        'y': 25,
+        'z': 26,
+
+        'A': 27,
+        'B': 28,
+        'C': 29,
+        'D': 30,
+        'E': 31,
+        'F': 32,
+        'G': 33,
+        'H': 34,
+        'I': 35,
+        'J': 36,
+        'K': 37,
+        'L': 38,
+        'M': 39,
+        'N': 40,
+        'O': 41,
+        'P': 42,
+        'Q': 43,
+        'R': 44,
+        'S': 45,
+        'T': 46,
+        'U': 47,
+        'V': 48,
+        'W': 49,
+        'X': 50,
+        'Y': 51,
+        'Z': 52,
+    }
+
+    with open(filename) as f:
+        for line in f:
+            lines.append(line.rstrip())
+
+    for i in range(0, len(lines), 3):
+        same_element = list(set(lines[i]).intersection(
+            lines[i + 1]).intersection(lines[i + 2]))
+        sum_of_values += my_dict[same_element[0]]
         same_element.clear()
 
     print(sum_of_values)
 
 
 rucksack_reorganization(filename)
+rucksack_reorganization_part2(filename)
